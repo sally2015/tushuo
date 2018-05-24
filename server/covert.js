af7e19f597cba11b34a6c73d5cee8b3068d29566
bingou1 <740100506@qq.com>
2018-05-24 08:07:39 +0800

    test covert

--- server/covert.js
+++ server/covert.js
@@ -1,21 +1,15 @@
-const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const ffmpeg = require('fluent-ffmpeg');
-
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
-// console.log(ffmpegInstaller.path, ffmpegInstaller.version);
-
-var track = './test.mp3';//your path to source file
-ffmpeg(track)
-.format('wav')
-.on('error', function (err) {
-    console.log('An error occurred: ' + err.message);
-})
-.on('progress', function (progress) {
-    // console.log(JSON.stringify(progress));
-    console.log('Processing: ' + progress.targetSize + ' KB converted');
-})
-.on('end', function () {
-    console.log('Processing finished !');
-})
-.save('./hello.wav');//path where you want to save your file
-
+const source = 'test.mp3';
+const target = 'output.mp3';
+ffmpeg(source)
+    .format('wav')
+    .on('error', function(err) {
+        reject(err.message);
+    })
+    .on('progress', function(progress) {
+        console.log('Processing: ' + progress.targetSize + ' KB converted');
+    })
+    .on('end', function() {
+        resolve(target);
+    })
+    .save(target);
