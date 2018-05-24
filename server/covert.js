f2febc9b30f3dc37478704be8e99e5ebf13c86a4
bingou1 <740100506@qq.com>
2018-05-24 08:15:23 +0800

    plus Installer

--- server/covert.js
+++ server/covert.js
@@ -1,15 +1,36 @@
+// const ffmpeg = require('fluent-ffmpeg');
+// const source = 'test.mp3';
+// const target = 'output.mp3';
+// ffmpeg(source)
+//     .format('wav')
+//     .on('error', function(err) {
+//         console.error(err.message);
+//     })
+//     .on('progress', function(progress) {
+//         console.log('Processing: ' + progress.targetSize + ' KB converted');
+//     })
+//     .on('end', function() {
+//         console.log(target);
+//     })
+//     .save(target);
+
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const ffmpeg = require('fluent-ffmpeg');
-const source = 'test.mp3';
-const target = 'output.mp3';
-ffmpeg(source)
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+// console.log(ffmpegInstaller.path, ffmpegInstaller.version);
+
+var track = './test.mp3'; //your path to source file
+ffmpeg(track)
     .format('wav')
     .on('error', function(err) {
-        console.error(err.message);
+        console.log('An error occurred: ' + err.message);
     })
     .on('progress', function(progress) {
+        // console.log(JSON.stringify(progress));
         console.log('Processing: ' + progress.targetSize + ' KB converted');
     })
     .on('end', function() {
-        console.log(target);
+        console.log('Processing finished !');
     })
-    .save(target);
+    .save('./hello.wav'); //path where you want to save your file
