388873862828590e7b47a13acbca992b76dde677
bingou1 <740100506@qq.com>
2018-05-24 08:12:38 +0800

    test covert

--- server/covert.js
+++ server/covert.js
@@ -4,12 +4,12 @@ const target = 'output.mp3';
 ffmpeg(source)
     .format('wav')
     .on('error', function(err) {
-        reject(err.message);
+        console.error(err.message);
     })
     .on('progress', function(progress) {
         console.log('Processing: ' + progress.targetSize + ' KB converted');
     })
     .on('end', function() {
-        resolve(target);
+        console.log(target);
     })
     .save(target);
